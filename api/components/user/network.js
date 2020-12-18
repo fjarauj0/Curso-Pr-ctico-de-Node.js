09bde8d595299839915fdaf0a9110fd69fbf84e6
Francisco Araujo <fjaraujo.dev@gmail.com>
2020-12-17 22:53:26 -0600

    Aislar el código de la base de datos

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -2,9 +2,11 @@ const express = require("express");
 const response = require("../../../network/response");
 
 const router = express.Router();
+const Controller = require("./controller");
 
 router.get("/", function (req, res) {
-  response.succes(req, res, "Todo funciona", 200);
+  const lista = Controller.list();
+  response.succes(req, res, lista, 200);
 });
 
 module.exports = router;
